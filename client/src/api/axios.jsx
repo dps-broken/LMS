1e5fcb1d3ba0874d0840f6a39f8849b484648512
Pranjal Kumar Dwivedi <pranjald@websoptimization.com>
2025-08-11 17:29:53 +0530

    Axios Url set

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || '/',
+    baseURL: "https://lms-kcpr.onrender.com", 
+    // For Development
+    // baseURL: import.meta.env.VITE_API_URL || '/',
     headers: {
         'Content-Type': 'application/json',
     },
